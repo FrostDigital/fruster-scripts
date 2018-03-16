b1ec7b0f2fd0d230d9238a9c18f6dcaf84b5b6cf
Joel Soderstrom <joel.soderstrom@gmail.com>
2018-03-16 20:34:59 +0100

    Install script

--- install/fruster-install.sh
+++ install/fruster-install.sh
@@ -7,18 +7,21 @@
 
 mkdir -p /usr/local/fruster && cd /usr/local/fruster
 
-git clone --depth 1 -f https://github.com/FrostDigital/fruster-script.git
+git clone --depth 1 -f https://github.com/FrostDigital/fruster-scripts.git
 
+# Remove old versions
 rm -f /usr/local/bin/fruster-create-kube
 rm -f /usr/local/bin/fruster-install-deis
 rm -f /usr/local/bin/fruster-install-nats
 
-chmod +x scripts/fruster-create-kube
+#chmod +x scripts/fruster-create-kube
 #chmod +x scripts/fruster-install-deis
 #chmod +x scripts/fruster-install-nats
 
 # Link binaries into /usr/local/bin
 ln -s scripts/fruster-create-kube /usr/local/bin/fruster-create-kube
+ln -s scripts/fruster-install-deis /usr/local/bin/fruster-install-deis
+ln -s scripts/fruster-install-nats /usr/local/bin/fruster-install-natss
 
 
 
