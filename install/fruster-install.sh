1cd5bba720c46a5a0be08f675c627570f9c5f985
Joel Soderstrom <joel.soderstrom@gmail.com>
2018-03-21 16:57:00 +0100

    cURL based installation instead of git

--- install/fruster-install.sh
+++ install/fruster-install.sh
@@ -5,13 +5,25 @@
 # 
 # This script installs to /usr/local only.
 
+#set -o xtrace
+
+TEMP_DIR=$(mktemp -d)
 TARGET_DIR=/usr/local/fruster
 BIN_DIR=/usr/local/bin
+ZIP_PACKAGE=$TEMP_DIR/fruster-scripts.zip
 
 rm -rf $TARGET_DIR
 mkdir -p $TARGET_DIR
 
-git clone --depth 1 https://github.com/FrostDigital/fruster-scripts.git $TARGET_DIR
+echo "Downloading package from github..."
+
+curl -sLk -o $ZIP_PACKAGE https://github.com/FrostDigital/fruster-scripts/archive/master.zip
+
+echo "Extracting..."
+
+echo $TEMP_DIR
+unzip $ZIP_PACKAGE -d $TEMP_DIR  
+cp -R $TEMP_DIR/fruster-scripts-master/* $TARGET_DIR
 
 # Remove old versions
 rm -f $BIN_DIR/fruster-create-kube
@@ -19,12 +31,15 @@ rm -f $BIN_DIR/fruster-install-deis
 rm -f $BIN_DIR/fruster-install-nats
 rm -f $BIN_DIR/fruster-switch
 
+echo "Linking..."
+
 # Link binaries into /usr/local/bin
 ln -s $TARGET_DIR/scripts/fruster-create-kube $BIN_DIR/fruster-create-kube
 ln -s $TARGET_DIR/scripts/fruster-install-deis $BIN_DIR/fruster-install-deis
 ln -s $TARGET_DIR/scripts/fruster-install-nats $BIN_DIR/fruster-install-nats
 ln -s $TARGET_DIR/scripts/fruster-switch $BIN_DIR/fruster-switch
 
+echo "Installation complete!"
 
 
 
